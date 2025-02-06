1d8f93e4bcb5f27287a518483391d2520b7955e9
Rebellant <158089230+Rebellant@users.noreply.github.com>
2025-02-06 03:07:23 +0100

    Update 6
    -Resubida después del save de la clase Game

--- app/src/main/java/dam/psp/GameCenter/Model/Game.java
+++ app/src/main/java/dam/psp/GameCenter/Model/Game.java
@@ -1,4 +1,51 @@
 package dam.psp.GameCenter.Model;
 
-public record Game(String image, String name, String url, boolean fav) {
+public class Game {
+
+    private String image;
+    private String name;
+    private String url;
+    boolean fav;
+
+    public Game(String image, String name, String url, boolean fav) {
+        this.image = image;
+        this.name = name;
+        this.url = url;
+        this.fav = fav;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isFav() {
+        return fav;
+    }
+
+    public void setFav(boolean fav) {
+        this.fav = fav;
+    }
+    
+    
+
 }
